31bb98256c8f2120a5dcf6cb5e0d60e2c0a48492
Abrish-Green <abrham365muche@gmail.com>
2022-09-15 06:50:48 +0300

    Built

--- Components/Warning/Index.jsx
+++ Components/Warning/Index.jsx
@@ -4,24 +4,22 @@ import React from "react";
 const Index = () => {
   return (
     <>
-      <div className="warning w-full h-full absolute top-0 left-0 bg-black text-white flex justify-center p-64">
+      <div className="absolute top-0 left-0 flex justify-center w-full h-full p-64 text-white bg-black warning">
         <div className="content">
-          <div className="warning-text flex justify-center text-6xl text-red-600 uppercase font-bold">
+          <div className="flex justify-center text-6xl font-bold text-red-600 uppercase warning-text">
             Warning
           </div>
-          <div className="warning-detail pt-10">
+          <div className="pt-10 warning-detail">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
             dicta ab modi minima neque, voluptate quisquam eligendi totam
             perspiciatis veritatis ducimus eum, pariatur voluptatibus provident
             quis nostrum nihil debitis aliquid?
           </div>
         </div>
-        <div className="no-smoking">
+        <div className="bg-white rounded-full h-28 no-smoking w-60">
           <img
             src="https://fontawesomeicons.com/lib/svg/logo-no-smoking.svg"
             alt="No smoking"
-            width="200"
-            height="100"
           />
         </div>
       </div>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-@type {import('next').NextConfig} 
+import('next').NextConfig 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
