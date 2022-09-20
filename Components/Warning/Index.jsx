f22ce1008cd2a288e72141e8836f03274998ec6d
Abrish-Green <abrham365muche@gmail.com>
2022-09-20 20:51:08 +0300

    Added the Redirect

--- Components/Warning/Index.jsx
+++ Components/Warning/Index.jsx
@@ -4,16 +4,21 @@ import React from "react";
 const Index = () => {
   return (
     <>
-      <div className="absolute top-0 left-0 flex justify-center w-full h-full p-64 text-white bg-black warning">
+      <div className="absolute h-[100vh] md:top-0 left-0 flex justify-center w-full p-14 md:p-24 lg:p-36 text-white bg-black warning">
         <div className="content">
           <div className="flex justify-center text-6xl font-bold text-red-600 uppercase warning-text">
             Warning
           </div>
-          <div className="pt-10 warning-detail">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-            dicta ab modi minima neque, voluptate quisquam eligendi totam
-            perspiciatis veritatis ducimus eum, pariatur voluptatibus provident
-            quis nostrum nihil debitis aliquid?
+          <div className="pt-8 warning-detail font-[Montserrat] text-justify text-xl tracking-wider ">
+            These warnings prevent audiences from stumbling blindly into a scene
+            that might rouse past traumas or scenes that they simply don't want
+            to read about.
+            <span className="italic tracking-wider">
+              {" "}
+              Some common and critical warnings are lots of questions about
+              salary, promotions,sick pay, benefits, Lack of trust, Narcissism,
+              Disrespecting Boundaries, Lack of Communication...
+            </span>
           </div>
         </div>
       </div>
---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+          <Head>
+        <link  href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,600;0,800;1,100;1,200;1,300;1,400;1,600;1,700&family=Mouse+Memoirs&family=Roboto+Condensed:wght@300&family=Rubik&family=Trirong:ital,wght@0,100;1,100;1,300&family=Work+Sans:ital,wght@1,300&display=swap" rel="stylesheet" />
+              
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
